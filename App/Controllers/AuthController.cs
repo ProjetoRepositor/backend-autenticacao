2cd60504ac5169a4e74968b74f75be547c5d0a21
Caio Ruiz <cerz@gft.com>
2023-09-18 09:10:46 -0300

    Adicionado middleware de autenticação

---
+++ App/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using App.DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Controllers;
+
+[Route("/api/autenticacao/v1/[controller]")]
+public class AuthController: ControllerBase
+{
+    [HttpGet]
+    [HttpPost]
+    [HttpPut]
+    [HttpDelete]
+    [HttpHead]
+    [HttpOptions]
+    [HttpPatch]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Autenticar([FromHeader] string authorize)
+    {
+        #region Verificando validade da sessão
+
+        var token = authorize.Replace("Bearer ", "");
+        
+        await using var contexto = new Contexto();
+
+        var sessao = await contexto.sessao.Where(s => s.HashSessao == token).FirstOrDefaultAsync();
+
+        if (
+            sessao != null &&
+            (
+                sessao.ManterLogin ||
+                sessao.UltimoAcesso > DateTime.Now.AddHours(-3)
+            )
+        ) return Ok();
+
+        return Unauthorized();
+        
+        #endregion
+    }
+}
